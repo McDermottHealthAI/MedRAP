38dd4ff161b40b8004d2c4becf97a34fee52b386
egillax <egillax@gmail.com>
2026-03-16 13:23:46 +0100

    feat: simplify hf dataset retriever runtime path

--- src/medrap/__init__.py
+++ src/medrap/__init__.py
@@ -16,7 +16,6 @@ from .pooling import IdentityPooling, MaskedMeanPooling
 from .query_projection import LinearQueryProjector, QueryProjector, SequenceMeanQueryProjector
 from .retrievers import (
     HFDatasetRetriever,
-    HFDatasetSnapshotBuilder,
     InMemoryRetriever,
     Retriever,
     load_in_memory_retriever,
@@ -38,7 +37,6 @@ __all__ = [
     "FusionInput",
     "FusionOutput",
     "HFDatasetRetriever",
-    "HFDatasetSnapshotBuilder",
     "IdentityPooling",
     "InMemoryRetriever",
     "InMemoryRetrieverConfig",
