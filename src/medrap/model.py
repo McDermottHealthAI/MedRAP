168bf5ea4b2bf043c1221547c40ebbdd53ae7fbd
Hayk Stepanyan hs3627@columbia.edu mm6677_gp Biomedical_Infomatics <haykstepanyan02@gmail.com>
2026-04-04 19:18:56 -0400

    successful single task experiment
    X
    ^X

--- src/medrap/model.py
+++ src/medrap/model.py
@@ -1,5 +1,6 @@
 """RAP API model orchestration."""
 
+import torch.nn.functional as F
 from meds_torchdata import MEDSTorchBatch
 from torch import Tensor, nn
 from torch.nn import functional as nn_functional
